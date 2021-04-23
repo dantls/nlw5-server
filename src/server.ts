3fdd856283204ed73057492d894172cdf4368064
dantls <dan_tls@hotmail.com>
2021-04-23 18:09:05 -0400

    Init websocket

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 import express from "express";
+import {createServer} from 'http';
+import {Server, Socket} from 'socket.io';
+
+import 'reflect-metadata';
 
 import cors from 'cors';
 
 import { routes } from "./routes";
 
-
-import 'reflect-metadata';
-
 import "./database";
 
+
 const app = express();
+
+const http = createServer(app); //Separando o protocolo http do express
+const io = new Server(http) //iniciando um servidor do protocolo do web socket
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou",socket.id)
+})
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +26,6 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333, ()=> {
+http.listen(3333, ()=> {
   console.log("Server is running on port 3333");
 })
